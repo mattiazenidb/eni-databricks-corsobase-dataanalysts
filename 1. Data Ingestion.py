ef3dcc4838def36468bdcea1fb1f73fb7b64fcec
mattiazenidb <mattiazenidb>
2024-05-23 14:11:09 +0000

    Updated notebook 1 and resources

--- 1. Data Ingestion.py
+++ 1. Data Ingestion.py
@@ -1,4 +1,11 @@
 # Databricks notebook source
 # MAGIC %md
+# MAGIC ### Instructions
+# MAGIC
+# MAGIC We want to upload file ```status_1.json``` manually into Volume ```/Volumes/eni_databricks_corsobase_dataanalysts/course_schema/landing``` and create a table ```historical_turbine_status``` in ```eni_databricks_corsobase_dataanalysts.course_schema``` from it with the UI
+
+# COMMAND ----------
+
+# MAGIC %md
 # MAGIC
 # MAGIC <img src="https://github.com/mattiazenidb/eni-workshop/raw/main/_resources/dataingestion_2.gif" style="float:right; margin-left: 10px" />
