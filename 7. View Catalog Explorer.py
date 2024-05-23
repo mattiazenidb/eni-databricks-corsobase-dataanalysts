4e1c89e0d081d27e6bbae381825c94b6d157e5b4
mattiazenidb <mattiazenidb>
2024-05-23 15:39:30 +0000

    Notebooks 6-8-9 are only placeholders for a live demo

---
+++ 7. View Catalog Explorer.py
@@ -0,0 +1,16 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC <img style="float:right; margin-left: 10px" src="https://github.com/mattiazenidb/eni-workshop/raw/main/_resources/bi.png" />
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC <img src="https://github.com/mattiazenidb/eni-workshop/raw/main/_resources/explorer_2.gif" style="float:right; margin-left: 10px" />
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC <img src="https://github.com/mattiazenidb/eni-workshop/raw/main/_resources/lineage.gif" style="float:right; margin-left: 10px" />
