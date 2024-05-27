83058a7a9abc60029f109044354fc3676960db70
mattiazenidb <mattiazenidb>
2024-05-27 05:42:36 +0000

    Moved commands from data analysts to data engineers

--- 0. Utils - Data Analysts.py
+++ 0. Utils - Data Analysts.py
@@ -26,20 +26,3 @@ schema = json.loads(dbutils.notebook.entry_point.getDbutils().notebook().getCont
 # COMMAND ----------
 
 spark.sql(f'CREATE SCHEMA IF NOT EXISTS {catalog}.{schema}')
-
-# COMMAND ----------
-
-dbutils.fs.mkdirs('/Volumes/landing/power/turbine_raw_landing/engines')
-dbutils.fs.mkdirs('/Volumes/landing/power/turbine_raw_landing/historical_turbine_status')
-dbutils.fs.mkdirs('/Volumes/landing/power/turbine_raw_landing/incoming_data')
-dbutils.fs.mkdirs('/Volumes/landing/power/turbine_raw_landing/parts')
-dbutils.fs.mkdirs('/Volumes/landing/power/turbine_raw_landing/turbines')
-
-# COMMAND ----------
-
-import shutil
-
-shutil.copyfile('_resources/engine_1.json', '/Volumes/landing/power/turbine_raw_landing/engines/engine_1.json')
-shutil.copyfile('_resources/parts.json', '/Volumes/landing/power/turbine_raw_landing/parts/parts.json')
-shutil.copyfile('_resources/part-00000-tid-5635562292802017934-c3e75aed-50bc-4000-bf88-2d2ef34bd1d9-130-1-c000.csv', '/Volumes/landing/power/turbine_raw_landing/incoming_data/part-00000-tid-5635562292802017934-c3e75aed-50bc-4000-bf88-2d2ef34bd1d9-130-1-c000.csv')
-shutil.copyfile('_resources/turbine_1.csv', '/Volumes/landing/power/turbine_raw_landing/turbines/turbine_1.json')
